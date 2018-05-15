58a8482fa1fff064542ede6267cc60dc572ad890
Francis Badasu <francisbadasu@Superman-MBP.connectify>
2018-05-15 12:02:34 +0000

    added siama and lazysizes
    Siama for the image sliders and lazysizes for lazy load of images

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-for (const a of document.querySelectorAll('a[href*="#"]')) {
-	a.addEventListener('click', ev => {
-		document
-			.getElementById(a.hash.substr(1))
-			.scrollIntoView({ behavior: 'smooth' })
+function smoothScroll(ev) {
+	const section = document.getElementById(ev.currentTarget.hash.substr(1))
+	if (section !== null) {
 		ev.preventDefault()
-	})
+		section.scrollIntoView({ behavior: 'smooth' })
+	}
 }
 
+    const siema = new Siema({ selector: '.carousel', duration: 400, loop: true });
+    setInterval(() => {
+        siema.next()
+	}, 4000);
+	
+	document.querySelector('.prev').addEventListener('click', () => siema.prev());
+	document.querySelector('.next').addEventListener('click', () => siema.next());
+	
 
